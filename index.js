19b533053afc8584462a51e307ec98e6435fab1d
Sneha0024 <77921781+Sneha0024@users.noreply.github.com>
2021-02-17 23:17:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-//movement animation to happen 
-const card = document.querySelector(" .card");
-const container = document.querySelector(" .container");
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
 
-//moving animation event 
-card.addEventListener( "mousemove" , (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-    let yAxis = (window.innerWidth / 2 - e.pageY) / 10;
-    card.style.transform = 'rotateY(${xAxis}deg) rotateX(${yAxis}deg)';
-}
-);
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
 //Animate In
 container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
